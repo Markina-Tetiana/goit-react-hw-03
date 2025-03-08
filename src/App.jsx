42969ea8d75203c80046e2e8257f61a045444c0b
Tetiana <ystritsam@gmail.com>
2025-03-08 20:16:33 +0100

    hw 3

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+
+function App() {
+  return (
+    <>
+      <h1>PhoneBook</h1>
+      <ContactForm />
+    </>
+  );
+}
+
+export default App;
